dc335284e808b08da74ab99b639d32d065b1a77f
BeLieVer031 <sandyrajak031@gmail.com>
2025-07-03 22:43:54 +0530

    fix: extra move and winner logic

--- tic-tac-toe/app.js
+++ tic-tac-toe/app.js
@@ -1,23 +1,36 @@
 const boxes = document.querySelectorAll(".box")
+const winner = document.querySelector(".winner")
+
 let turn = "X";
-let movesArr = [];
+let movesArr = ["", "", "", "", "", "", "", "", ""];
 
 for (let i = 0; i < boxes.length; i++) {
     boxes[i].addEventListener("click", function () {
-        boxes[i].innerHTML = turn
-        movesArr[i] = turn;
 
-        if (movesArr[0] === movesArr[1] && movesArr[1] === movesArr[2]) {
-            console.log(turn + " is winner.");
-        } else if (movesArr[3] === movesArr[4] && movesArr[4] === movesArr[5]) {
-            console.log(turn + " is winner.");
+        if (boxes[i].innerHTML === "") {
+            boxes[i].innerHTML = turn
+            movesArr[i] = turn;
+
+            if (
+                movesArr[0] !== "" && movesArr[1] !== "" && movesArr[2] !== "" &&
+                movesArr[0] === movesArr[1] && movesArr[1] === movesArr[2]
+            ) {
+                winner.innerHTML = turn + " is winner."
+            } else if (
+                movesArr[3] !== "" && movesArr[4] !== "" && movesArr[5] !== "" &&
+                movesArr[3] === movesArr[4] && movesArr[4] === movesArr[5]
+            ) {
+                winner.innerHTML = turn + " is winner."
+            }
+
+            if (turn === "X") {
+                turn = "O"
+            } else {
+                turn = "X"
+            }
         }
 
-        if (turn === "X") {
-            turn = "O"
-        } else {
-            turn = "X"
-        }
+
 
     })
 }
