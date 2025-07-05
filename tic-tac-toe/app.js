a427d0c1ade9e0cb7b3eb907b01ba6c41b2e5eed
BeLieVer031 <sandyrajak031@gmail.com>
2025-07-05 20:55:47 +0530

    feat: game completed

--- tic-tac-toe/app.js
+++ tic-tac-toe/app.js
@@ -1,11 +1,15 @@
+let Showing = document.querySelector(".ShowingTurn")
 const boxes = document.querySelectorAll(".box")
 const winner = document.querySelector(".winner")
+let Reset = document.querySelector(".ResetButton")
 
 let turn = "X";
 let movesArr = ["", "", "", "", "", "", "", "", ""];
+Showing.innerHTML = "It is " + turn + "'s turn"
 
 for (let i = 0; i < boxes.length; i++) {
     boxes[i].addEventListener("click", function () {
+        console.log(45);
 
         if (boxes[i].innerHTML === "") {
             boxes[i].innerHTML = turn
@@ -16,21 +20,76 @@ for (let i = 0; i < boxes.length; i++) {
                 movesArr[0] === movesArr[1] && movesArr[1] === movesArr[2]
             ) {
                 winner.innerHTML = turn + " is winner."
-            } else if (
+            }
+
+            else if (
                 movesArr[3] !== "" && movesArr[4] !== "" && movesArr[5] !== "" &&
                 movesArr[3] === movesArr[4] && movesArr[4] === movesArr[5]
             ) {
                 winner.innerHTML = turn + " is winner."
             }
 
+            else if (
+                movesArr[6] !== "" && movesArr[7] !== "" && movesArr[8] !== "" &&
+                movesArr[6] === movesArr[7] && movesArr[7] === movesArr[8]
+            ) {
+                winner.innerHTML = turn + " is winner."
+            }
+
+            else if (
+                movesArr[0] !== "" && movesArr[3] !== "" && movesArr[6] !== "" &&
+                movesArr[0] === movesArr[3] && movesArr[3] === movesArr[6]
+            ) {
+                winner.innerHTML = turn + " is winner."
+            }
+
+            else if (
+                movesArr[1] !== "" && movesArr[4] !== "" && movesArr[7] !== "" &&
+                movesArr[1] === movesArr[4] && movesArr[4] === movesArr[7]
+            ) {
+                winner.innerHTML = turn + " is winner."
+            }
+
+            else if (
+                movesArr[2] !== "" && movesArr[5] !== "" && movesArr[8] !== "" &&
+                movesArr[2] === movesArr[5] && movesArr[5] === movesArr[8]
+            ) {
+                winner.innerHTML = turn + " is winner."
+            }
+
+            else if (
+                movesArr[0] !== "" && movesArr[4] !== "" && movesArr[8] !== "" &&
+                movesArr[0] === movesArr[4] && movesArr[4] === movesArr[8]
+            ) {
+                winner.innerHTML = turn + " is winner."
+            }
+
+            else if (
+                movesArr[2] !== "" && movesArr[4] !== "" && movesArr[6] !== "" &&
+                movesArr[2] === movesArr[4] && movesArr[4] === movesArr[6]
+            ) {
+                winner.innerHTML = turn + " is winner."
+            }
+
+
             if (turn === "X") {
                 turn = "O"
+                Showing.innerHTML = "It is " + turn + "'s turn"
+
             } else {
                 turn = "X"
+                Showing.innerHTML = "It is " + turn + "'s turn"
             }
         }
 
-
-
     })
 }
+
+Reset.addEventListener("click", function () {
+    for (let i = 0; i < boxes.length; i++) {
+        boxes[i].innerHTML = "";
+        movesArr = ["", "", "", "", "", "", "", "", ""];
+        turn = "X";
+        winner.innerHTML = "";
+    }
+})
