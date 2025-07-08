2131a808ba2e533229e67aee3a49b0e91822a325
BeLieVer031 <sandyrajak031@gmail.com>
2025-07-08 21:57:47 +0530

    feat: fully coded

--- todo/app.js
+++ todo/app.js
@@ -1,1 +1,26 @@
-console.log(31);
+const input = document.querySelector("input")
+const addBtn = document.querySelector("#add-btn")
+const taskArea = document.querySelector(".task-area")
+
+addBtn.addEventListener("click", function () {
+
+    if (input.value === "") return;
+
+    const taskDiv = document.createElement("div")
+    taskDiv.classList.add("task");
+    const taskPara = document.createElement("p")
+    const deleteBtn = document.createElement("button")
+    deleteBtn.innerHTML = "delete"
+    taskPara.innerHTML = input.value;
+
+    taskDiv.appendChild(taskPara)
+    taskDiv.appendChild(deleteBtn)
+
+    taskArea.appendChild(taskDiv)
+
+    input.value = "";
+
+    deleteBtn.addEventListener("click", function () {
+        taskDiv.remove()
+    })
+})
