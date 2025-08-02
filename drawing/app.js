5baa2d394258f58121a7703a11deaf9bc605ca0c
BeLieVer031 <sandyrajak031@gmail.com>
2025-08-02 23:39:51 +0530

    feat: add rect

--- drawing/app.js
+++ drawing/app.js
@@ -2,8 +2,12 @@ const canvasElem = document.querySelector("canvas")
 
 const canvas = new fabric.Canvas(canvasElem, {
     width: window.innerWidth,
-    height: window.innerHeight
+    height: window.innerHeight,
 })
+
+// has to fix
+// canvas.defaultCursor = 'crosshair';
+
 
 const iconsBtn = document.querySelectorAll(".floating-div *")
 let prevSelectedElem = iconsBtn[0]
@@ -15,3 +19,31 @@ for (let i = 0; i < iconsBtn.length; i++) {
         prevSelectedElem = iconsBtn[i]
     })
 }
+
+let x1 = 0;
+let y1 = 0;
+
+canvas.on("mouse:down", function () {
+
+    const pos = canvas.getPointer()
+    x1 = pos.x
+    y1 = pos.y
+
+    const rect = new fabric.Rect({
+        top: y1,
+        left: x1,
+        width: 10,
+        height: 10,
+        stroke: "black",
+    })
+
+    canvas.add(rect)
+})
+
+canvas.on("mouse:move", function () {
+    // console.log(canvas.getPointer());
+})
+
+canvas.on("mouse:up", function () {
+    console.log("i am x2 & y2", canvas.getPointer());
+})
